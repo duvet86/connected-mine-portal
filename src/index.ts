e12b931f541bd241326572133aaf4f50fbd3a916
Luca Marangon <Luca_Marangon@Trimble.com>
2019-03-08 16:58:49 +0800

    Working.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { MDCTopAppBar } from "@material/top-app-bar/index";
+import { MDCRipple } from "@material/ripple/index";
 
 // Instantiation
 const topAppBarElement = document.querySelector(".mdc-top-app-bar");
 if (topAppBarElement) {
-  const topAppBar = new MDCTopAppBar(topAppBarElement);
+  MDCTopAppBar.attachTo(topAppBarElement);
+}
+
+const button = document.getElementById("my-button");
+if (button != null) {
+  MDCRipple.attachTo(button);
 }
