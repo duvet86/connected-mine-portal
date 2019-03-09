314a194676bc94a0eb1114dc42e3081e2f0b7b55
Luca <duvet86@gmail.com>
2019-03-09 10:51:27 +0800

    Working.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
-import { MDCTopAppBar } from "@material/top-app-bar/index";
-import { MDCRipple } from "@material/ripple/index";
+import { MDCTopAppBar } from "@material/top-app-bar";
+import { MDCRipple } from "@material/ripple";
 import { MDCDrawer } from "@material/drawer";
 
 // Instantiation
 const button = document.getElementById("my-button");
-if (button != null) {
+if (button) {
   MDCRipple.attachTo(button);
 }
 
 const topAppBarElement = document.querySelector(".mdc-top-app-bar");
 const drawer = document.querySelector(".mdc-drawer");
 if (topAppBarElement && drawer) {
-  if (drawer != null) {
-    const drawerMDC = MDCDrawer.attachTo(drawer);
-    drawerMDC.open = true;
+  const drawerMDC = MDCDrawer.attachTo(drawer);
+  drawerMDC.open = true;
 
-    const topAppBar = MDCTopAppBar.attachTo(topAppBarElement);
-    const mainContent = document.getElementById("main-content");
-    if (mainContent != null) {
-      topAppBar.setScrollTarget(mainContent);
-      topAppBar.listen("MDCTopAppBar:nav", () => {
-        drawerMDC.open = !drawerMDC.open;
-      });
-    }
+  const topAppBar = MDCTopAppBar.attachTo(topAppBarElement);
+  const mainContent = document.getElementById("main-content");
+  if (mainContent) {
+    topAppBar.setScrollTarget(mainContent);
+    topAppBar.listen("MDCTopAppBar:nav", () => {
+      drawerMDC.open = !drawerMDC.open;
+    });
   }
 }
