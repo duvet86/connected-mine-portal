0f600b7f252440ce64a75557291f42a4d75fe207
Luca <duvet86@gmail.com>
2019-03-09 10:50:04 +0800

    Working.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import { MDCTopAppBar } from "@material/top-app-bar/index";
 import { MDCRipple } from "@material/ripple/index";
+import { MDCDrawer } from "@material/drawer";
 
 // Instantiation
-const topAppBarElement = document.querySelector(".mdc-top-app-bar");
-if (topAppBarElement) {
-  MDCTopAppBar.attachTo(topAppBarElement);
-}
-
 const button = document.getElementById("my-button");
 if (button != null) {
   MDCRipple.attachTo(button);
+}
+
+const topAppBarElement = document.querySelector(".mdc-top-app-bar");
+const drawer = document.querySelector(".mdc-drawer");
+if (topAppBarElement && drawer) {
+  if (drawer != null) {
+    const drawerMDC = MDCDrawer.attachTo(drawer);
+    drawerMDC.open = true;
+
+    const topAppBar = MDCTopAppBar.attachTo(topAppBarElement);
+    const mainContent = document.getElementById("main-content");
+    if (mainContent != null) {
+      topAppBar.setScrollTarget(mainContent);
+      topAppBar.listen("MDCTopAppBar:nav", () => {
+        drawerMDC.open = !drawerMDC.open;
+      });
+    }
+  }
 }
